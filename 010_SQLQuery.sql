9b638ba3818a00df89fe3a80907b622c989d982c
Andriy Kravets <a-kravets@users.noreply.github.com>
2020-06-15 08:57:03 +0300

    Text functions

---
+++ 010_SQLQuery.sql
@@ -0,0 +1,112 @@
+﻿/*ЗАДАНИЕ 1.
+Напишите SQL-запрос, который выбирает из таблицы phones столбец os. Используя операцию сравнения like
+отобрать только те строки, которые начинаются с «android v»*/
+
+SELECT
+	os
+FROM phones
+WHERE os LIKE 'android v%'
+
+/*ЗАДАНИЕ 2.
+Используя функцию charindex() добавьте в запрос из задачи 1 столбец с номером позиции символа «.» в os*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+FROM phones
+WHERE os LIKE 'android v%'
+
+/*ЗАДАНИЕ 3.
+Используя функцию charindex() добавьте в запрос из задачи 2 столбец с номером позиции символа «v» в os*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+	,CHARINDEX('v', os) AS v_pos
+FROM phones
+WHERE os LIKE 'android v%'
+
+/*ЗАДАНИЕ 4.
+Используя функцию substring() добавьте в запрос из задачи 3 столбец c подстрокой из столбца os которая начинается
+с номера позиции символа «v» плюс 1 и имеет длину: номер позиции символа «.» минус номер позиции символа
+«v» минус 1. Дайте название столбцу*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+	,CHARINDEX('v', os) AS v_pos
+	,SUBSTRING(os, CHARINDEX('v', os) + 1, CHARINDEX('.', os) - (CHARINDEX('v', os))-1) AS ss_vdot
+FROM phones
+WHERE os LIKE 'android v%'
+
+/*ЗАДАНИЕ 5.
+Используя функцию substring() добавьте в запрос из задачи 4 столбец c подстрокой из столбца os которая начинается
+с номера позиции символа «.» плюс 1 длиной 1 символ. Дайте название столбцу*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+	,CHARINDEX('v', os) AS v_pos
+	,SUBSTRING(os, CHARINDEX('v', os) + 1, CHARINDEX('.', os) - (CHARINDEX('v', os))-1) AS ss_vdot
+	,SUBSTRING(os, CHARINDEX('.', os) + 1, 1) AS ss_dot_1
+FROM phones
+WHERE os LIKE 'android v%'
+
+/*ЗАДАНИЕ 6.
+В запрос из задачи 5 добавьте сортировку по двум последним добавленным столбцам*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+	,CHARINDEX('v', os) AS v_pos
+	,SUBSTRING(os, CHARINDEX('v', os) + 1, CHARINDEX('.', os) - (CHARINDEX('v', os))-1) AS ss_vdot
+	,SUBSTRING(os, CHARINDEX('.', os) + 1, 1) AS ss_dot_1
+FROM phones
+WHERE os LIKE 'android v%'
+ORDER BY ss_vdot, ss_dot_1
+
+/*ЗАДАНИЕ 7.
+В запросе из задачи 6 умножте два последних добавленных столбца на 1. Как изменился порядок сортировки?*/
+
+SELECT
+	os
+	,CHARINDEX('.', os) AS dot_pos
+	,CHARINDEX('v', os) AS v_pos
+	,(SUBSTRING(os, CHARINDEX('v', os) + 1, CHARINDEX('.', os) - (CHARINDEX('v', os))-1)) * 1 AS ss_vdot
+	,(SUBSTRING(os, CHARINDEX('.', os) + 1, 1)) * 1 AS ss_dot_1
+FROM phones
+WHERE os LIKE 'android v%'
+ORDER BY ss_vdot, ss_dot_1
+
+/*ЗАДАНИЕ 8.
+Напишите SQL-запрос, который группирует строки таблицы clients по первой букве столбца client_name. (Группировку
+необходимо делать по вычисляемому с помощью функции столбцу). Добавте сортировку строк полученного запроса*/
+
+SELECT
+	SUBSTRING(client_name, 1, 1)
+FROM clients
+GROUP BY SUBSTRING(client_name, 1, 1)
+ORDER BY SUBSTRING(client_name, 1, 1)
+
+/*ЗАДАНИЕ 9.
+Напишите SQL-запрос, который выбирает из таблицы clients строки, где первые 5 символов столбца client_name
+читаются одинаково с права на лево. Использовать функцию REVERSE()*/
+
+SELECT
+	client_name
+	,SUBSTRING(client_name, 1, 5) AS ss
+	,REVERSE(SUBSTRING(client_name, 1, 5)) AS r_ss
+FROM clients
+WHERE SUBSTRING(client_name, 1, 5) = REVERSE(SUBSTRING(client_name, 1, 5))
+
+
+/*ЗАДАНИЕ 10.
+Используя функцию replace() добавьте в запрос из задачи 1 новый столбец, который представляет собой столбец os с
+буквой «v» заменённой на «версия»*/
+
+SELECT
+	os
+	,REPLACE(os, 'v', 'версия') AS repl_v
+FROM phones
+WHERE os LIKE 'android v%'
+
---
+++ M010_SQLQuery_lecture_code.sql
@@ -0,0 +1,40 @@
+﻿USE P_STORE
+/*
+SELECT
+	phone_name
+	,memory
+	,CHARINDEX('GB', phone_name) AS pos_gb
+	,SUBSTRING(phone_name, CHARINDEX('GB', phone_name)-2, 2 )
+FROM
+	phones
+WHERE memory IS NULL
+	AND CHARINDEX('GB', phone_name) <> 0
+*/
+
+/*
+SELECT
+	phone_name
+	,review
+	,PATINDEX('%20__ г%', review) AS i_year
+	,SUBSTRING(review, PATINDEX('%20%', review), 4) AS year
+FROM phones
+WHERE review IS NOT NULL
+	AND PATINDEX('%201%', review) <> 0
+*/
+
+/*
+SELECT
+	b.brand_name
+	,f.phone_name
+	,CONCAT(UPPER(TRIM(b.brand_name)), SPACE(1), f.phone_name) AS brand_phone
+FROM
+	phones f
+	LEFT JOIN brands b ON b.brand_id = f.brand_id
+*/
+
+SELECT
+	color_name
+	,SUBSTRING(color_name, 2, LEN(color_name)-1) -- word except 1st letter
+	,UPPER(LEFT(color_name, 1)) -- first letter uppercase
+	,CONCAT(UPPER(LEFT(color_name, 1)), SUBSTRING(color_name, 2, LEN(color_name)-1))
+FROM colors
