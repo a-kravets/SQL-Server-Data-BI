89a8183a7c457512968fcc2cc47fad366b3c2311
Andriy Kravets <a-kravets@users.noreply.github.com>
2020-11-28 08:05:46 +0200

    adding DDL homework and CRM creation lecture code

---
+++ 016_SQLQuery.sql
@@ -0,0 +1,107 @@
+﻿/*ЗАДАНИЕ 1.
+C помощью меню SSMS создайте таблицу exchange_rate со столбцами cur_name nchar(3) и rate numeric(4,2). Вставте в неё строку со
+значениями 'USD' и 25*/
+
+SELECT * FROM exchange_rate
+
+INSERT INTO exchange_rate (cur_name, rate)
+VALUES ('USD', 25)
+
+
+/*ЗАДАНИЕ 2.
+Напишите SQL-запрос, который соединяет таблицы phone_price и exchange_rate неявным соединением. Запрос должен возвращать
+все столбцы из таблицы phone_price, кроме столбца price. Создайте вычисляемый столбец с выражением round(price/rate,0) as price.
+Создайте представление (VIEW) с именем phone_price_usd на основе написанного запроса*/
+
+CREATE VIEW phone_price_usd
+AS
+SELECT 
+	phone_id
+	,color_id
+	,merchant_id
+	,ROUND(price/rate, 0) AS price_usd
+FROM phone_price, exchange_rate
+
+
+/*ЗАДАНИЕ 3.
+Вставьте в таблицу exchange_rate новую строку со значениями 'EUR' и 27. Проверьте что возвращает запрос выбирающий все строки
+и столбцы из представления phone_price_usd*/
+
+INSERT INTO exchange_rate (cur_name, rate)
+VALUES ('EUR', 27)
+
+SELECT * FROM phone_price_usd
+
+/*ЗАДАНИЕ 4.
+Добавьте в запрос из задания 2 условие WHERE cur_name=’USD’. С помощью команды ALTER VIEW измените представление
+phone_price_usd на основе запроса с фильтром cur_name=’USD’*/
+
+ALTER VIEW phone_price_usd
+AS
+SELECT 
+	phone_id
+	,color_id
+	,merchant_id
+	,ROUND(price/rate, 0) AS price_usd
+FROM phone_price, exchange_rate
+WHERE cur_name = 'USD'
+
+/*ЗАДАНИЕ 5.
+Создайте представление с именем phone_price_eur, которое будет возвращать прайс-лист с ценами пересчитанными в
+евро*/
+
+CREATE VIEW phone_price_eur
+AS
+SELECT 
+	phone_id
+	,color_id
+	,merchant_id
+	,ROUND(price/rate, 0) AS price_eur
+FROM phone_price, exchange_rate
+WHERE cur_name = 'EUR'
+
+/*ЗАДАНИЕ 6.
+С помощью команды ALTER TABLE в таблицу exchange_rate добавьте (ADD) новые столбцы: date_from date и date_to
+date. Заполните столбец date_from значением DATEFROMPARTS(2020,1,1), а столбец date_to значением
+DATEFROMPARTS(2100,12,31) . Вставьте в таблицу exchange_rate две новые строки со значениями 'USD' ,24,
+DATEFROMPARTS(2019,1,1), DATEFROMPARTS(2019,12,31) и 'EUR' ,26, DATEFROMPARTS(2019,1,1) ,
+DATEFROMPARTS(2019,12,31) в столбцы cur_name,currency,date_from,date_to*/
+
+ALTER TABLE exchange_rate
+ADD date_from date,
+	date_to date;
+
+INSERT INTO exchange_rate (date_from, date_to)
+VALUES (DATEFROMPARTS(2020,1,1), DATEFROMPARTS(2100,12,31))
+
+INSERT INTO exchange_rate (cur_name, rate, date_from, date_to)
+VALUES	('USD' ,24, DATEFROMPARTS(2019,1,1), DATEFROMPARTS(2019,12,31)),
+		('EUR' ,26, DATEFROMPARTS(2019,1,1), DATEFROMPARTS(2019,12,31));
+
+SELECT * from exchange_rate
+
+/*ЗАДАНИЕ 7.
+Измените представления phone_price_usd и phone_price_eur так, чтобы в них содержались только те строки, которые
+соответствуют курсу валют на сегодня*/
+
+ALTER VIEW phone_price_usd
+AS
+SELECT 
+	phone_id
+	,color_id
+	,merchant_id
+	,ROUND(price/rate, 0) AS price_usd
+FROM phone_price, exchange_rate
+WHERE cur_name = 'USD'
+	AND GETDATE() BETWEEN date_from AND date_to
+
+ALTER VIEW phone_price_eur
+AS
+SELECT 
+	phone_id
+	,color_id
+	,merchant_id
+	,ROUND(price/rate, 0) AS price_usd
+FROM phone_price, exchange_rate
+WHERE cur_name = 'EUR'
+	AND GETDATE() BETWEEN date_from AND date_to
---
+++ M17_SQLQuery_lecture_code.sql
@@ -0,0 +1,86 @@
+﻿-- Будьте внимательны! Некорорые скрипты ниже приводят к изменениям данных в объектах БД
+-- а также к изменеию объектов БД
+
+-- 1. ********** Заполняем данными созданные таблицы **********************
+--insert into crm_operator select * from t4
+--insert into crm_evaluate select * from t3
+--insert into crm_cont_type select * from t2
+--insert into crm_contacts select * from t1
+
+
+-- Проверяем наличие вставленных данных
+select * from crm_operator
+select * from crm_evaluate
+select * from crm_cont_type
+select * from crm_contacts
+
+
+-- 2. ********** Создаем ограничения NOT NULL и первичные ключи ***********
+
+-- Ограничение NOT NULL и первичный ключ в таблице crm_operator:
+/*
+alter table crm_operator alter column operator_id smallint not null
+
+alter table crm_operator
+add constraint PK_Operator_Id primary key (operator_id)
+*/
+
+-- Ограничение NOT NULL и первичный ключ в таблице crm_evaluate:
+select * from crm_evaluate
+/*
+alter table crm_evaluate alter column evaluate_id smallint not null
+alter table crm_evaluate add constraint PK_evaluate_Id primary key (evaluate_id)
+*/
+
+-- Ограничение NOT NULL и первичный ключ в таблице crm_cont_type:
+select * from crm_cont_type
+/*
+alter table crm_cont_type alter column contact_id smallint not null
+alter table crm_cont_type add constraint PK_contact_Id primary key (contact_id)
+-- !!! ошибка из-за неуникальность значений в столбце contact_id
+*/
+-- исправление ошибки через создание промежуточной таблицы и удаление дубликатов
+--select distinct contact_id,io,contact_descriptor into crm_cont_type2 from crm_cont_type
+--delete from crm_cont_type
+--insert into crm_cont_type select * from crm_cont_type2
+--drop table crm_cont_type2
+select * from crm_cont_type
+
+-- создаем заново первичный ключ в таблице crm_cont_type:
+/*
+alter table crm_cont_type add constraint PK_contact_Id primary key (contact_id)
+*/
+
+
+-- 3. ***** Создание ограничений NOT NULL и внешних ключей в таблице crm_contacts ****
+select * from crm_contacts
+
+-- Создание ограничений NOT NULL для 3-х столбцов crm_contacts:
+/*
+alter table crm_contacts alter column date_cont smalldatetime not null
+alter table crm_contacts alter column client_id int not null
+alter table crm_contacts alter column contact_id smallint not null
+*/
+
+-- Создание 4 внешних ключа в таблице crm_contacts:
+/*
+alter table crm_contacts add constraint FK_client_Id foreign key (client_id)
+	references clients (client_id)
+
+alter table crm_contacts add constraint FK_evaluate_Id foreign key (evaluate_id)
+	references crm_evaluate (evaluate_id)
+
+alter table crm_contacts add constraint FK_operator_Id foreign key (operator_id)
+	references crm_operator (operator_id)
+
+alter table crm_contacts add constraint FK_contact_Id foreign key (contact_id)
+	references crm_cont_type (contact_id)
+*/
+
+
+-- 4. ************ Создание первичного ключа в таблице crm_contacts **************
+/*
+alter table crm_contacts add id int primary key identity
+*/
+
+select * from crm_contacts
