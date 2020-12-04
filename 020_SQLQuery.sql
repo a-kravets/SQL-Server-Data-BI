ed50be612a147eb8c27207262857770095d3e0e8
Andriy Kravets <a-kravets@users.noreply.github.com>
2020-12-04 08:51:53 +0200

    adding sys settings checkup

---
+++ 020_SQLQuery.sql
@@ -0,0 +1,23 @@
+﻿SELECT 'ANSI_NULLS' AS Parameter, SESSIONPROPERTY ('ANSI_NULLS') AS Value
+
+UNION
+
+SELECT 'Collation' AS Parameter, DatabasePropertyEx(db_name(),'Collation') AS Value
+
+UNION
+
+SELECT 'CONCAT_NULL_YIELDS_NULL' AS Parameter, SESSIONPROPERTY ('CONCAT_NULL_YIELDS_NULL') AS Value
+
+UNION
+
+SELECT 'QUOTED_IDENTIFIER' AS Parameter, SESSIONPROPERTY ('QUOTED_IDENTIFIER') AS Value
+
+UNION
+
+SELECT  'Date format' AS Parameter, (SELECT date_format
+FROM  sys.dm_exec_sessions 
+WHERE  session_id = @@spid) AS Value
+
+UNION
+
+SELECT 'Language' AS Parameter, @@LANGUAGE AS Value
